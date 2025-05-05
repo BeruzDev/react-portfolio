2adad3729d9f222f9dbeb2881805a402dcd3177b
BeruzDev <albertus1291@gmail.com>
2025-05-05 20:36:38 +0200

    Navbar⚙️

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,33 @@ import About from './pages/About/About.jsx'
 import Skills from './pages/Skills/Skills.jsx'
 import Projects from './pages/Projects/Projects.jsx'
 import Contact from './pages/Contact/Contact.jsx'
+import useTheme from './hooks/useTheme.js'
+import { Heart } from 'lucide-react'
 
 function App() {
+  const { theme, toggleTheme, isDark } = useTheme()
+
   return (
-    <div className='app'>
-      <Navbar />
+    <div className={`app ${theme}`}>
+      <Navbar toggleTheme={toggleTheme} isDark={isDark} />
       <main>
-          <section id="about"><About /></section>
-          <section id="skills"><Skills /></section>
-          <section id="projects"><Projects /></section>
-          <section id="contact"><Contact /></section>
+        <section id="about">
+          <About />
+        </section>
+        <section id="skills">
+          <Skills />
+        </section>
+        <section id="projects">
+          <Projects />
+        </section>
+        <section id="contact">
+          <Contact />
+        </section>
       </main>
+      <footer>
+        <p>Coded by BeruzDev</p>
+        <Heart size={10} strokeWidth={2} className='footer-icon'/>
+      </footer>
     </div>
   )
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,28 +1,66 @@
 import React from 'react'
 import '../../Styles/Navbar.css'
+import { Sun, SunMoon } from 'lucide-react'
 
-const Navbar = () => {
-	return (
-		<div className='navbar'>
-			<div className='logo'>
-				Albert Castro
-			</div>
-			<div className='menu'>
-				<button className='menu-btn' onClick={() => document.getElementById('about').scrollIntoView({ behavior: 'smooth'})}>
-					Sobre mí
-				</button>
-				<button className='menu-btn' onClick={() => document.getElementById('skills').scrollIntoView({ behavior: 'smooth'})}>
-					Habilidades
-				</button>
-				<button className='menu-btn' onClick={() => document.getElementById('projects').scrollIntoView({ behavior: 'smooth'})}>
-					Proyectos
-				</button>
-				<button className='menu-btn' onClick={() => document.getElementById('contact').scrollIntoView({ behavior: 'smooth'})}>
-					Contacto
-				</button>
-			</div>
-		</div>
-	)
+const Navbar = ({ toggleTheme, isDark }) => {
+  return (
+    <div className="navbar">
+      <div className="logo">Albert Castro</div>
+      <div className="menu">
+        <button
+          className="menu-btn"
+          onClick={() =>
+            document
+              .getElementById('about')
+              .scrollIntoView({ behavior: 'smooth' })
+          }
+        >
+          Sobre mí
+        </button>
+        <button
+          className="menu-btn"
+          onClick={() =>
+            document
+              .getElementById('skills')
+              .scrollIntoView({ behavior: 'smooth' })
+          }
+        >
+          Habilidades
+        </button>
+        <button
+          className="menu-btn"
+          onClick={() =>
+            document
+              .getElementById('projects')
+              .scrollIntoView({ behavior: 'smooth' })
+          }
+        >
+          Proyectos
+        </button>
+        <button
+          className="menu-btn"
+          onClick={() =>
+            document
+              .getElementById('contact')
+              .scrollIntoView({ behavior: 'smooth' })
+          }
+        >
+          Contacto
+        </button>
+        <button
+          className="menu-btn theme-btn"
+          onClick={toggleTheme}
+          aria-label="Toggle theme"
+        >
+          {isDark ? (
+            <Sun size={20} strokeWidth={1.25} />
+          ) : (
+            <SunMoon size={20} strokeWidth={1.25} />
+          )}
+        </button>
+      </div>
+    </div>
+  )
 }
 
 export default Navbar
