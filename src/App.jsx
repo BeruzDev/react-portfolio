99fa75057c5f9eead791906b28c89682b28343fc
BeruzDev <albertus1291@gmail.com>
2025-05-06 19:52:55 +0200

    Imports update

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import About from './pages/About/About.jsx'
 import Skills from './pages/Skills/Skills.jsx'
 import Projects from './pages/Projects/Projects.jsx'
 import Contact from './pages/Contact/Contact.jsx'
-import useTheme from './Hooks/useTheme.js'
+import useTheme from './hooks/useTheme.js'
 import { Heart } from 'lucide-react'
 
 function App() {
