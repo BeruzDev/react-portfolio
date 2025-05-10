4f4ed64206b6a7bb1e0772c2bfcc490bda0b3c79
BeruzDev <albertus1291@gmail.com>
2025-05-10 16:30:02 +0200

    Documents access update

--- src/hooks/useExperienceElement.js
+++ src/hooks/useExperienceElement.js
@@ -12,7 +12,7 @@ export const useExperienceElement = () => {
       button: 'Referencias',
       onclick: () =>
         window.open(
-          '/Documents/KENNWORT_CartaRecomendacionCastellano_AlbertCastroAlbacete.pdf'
+          '../../public/Documents/KENNWORT_CartaRecomendacionCastellano_AlbetCastroAlbacete.pdf'
         ),
     },
     {
@@ -23,7 +23,7 @@ export const useExperienceElement = () => {
       button: 'Recomendación',
       onclick: () =>
         window.open(
-          '/Documents/EDAG_CartaRecomendacion_AlbertCastroAlbacete_CENSORED.pdf'
+          '../../public/Documents/EDAG_CartaRecomendacion_AlbetCastroAlbacete_CENSORED.pdf'
         ),
     },
     {
@@ -34,7 +34,7 @@ export const useExperienceElement = () => {
       button: 'Expediente',
       onclick: () =>
         window.open(
-          '/Documents/finishedz_grade_certificate_CENSORED.pdf'
+          '../../public/Documents/finished_grade_certificate_CENSORED.pdf'
         ),
     },
   ]
