eb0646b47bf7dc06283ccf49fab1b1e672f6c957
BeruzDev <albertus1291@gmail.com>
2025-05-16 16:36:07 +0200

    Contact section done✅

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,18 +1,38 @@
 import React from 'react'
 import '../../styles/Contact.css'
+import ContactForm from '../../components/ContactForm/ContactForm'
+import GmailIcon from '../../assets/Icons/Logos/gmail.svg?react'
+import LinkedinIcon from '../../assets/Icons/Logos/linkedin.svg?react'
 
 const Contact = () => {
-	return (
-		<div className="section">
+  return (
+    <div className="section">
       <div className="name-section">
         <p className="section-subtitle">Quedo a su disposición</p>
         <p className="section-title">Contáctame</p>
       </div>
       <div className="contact-cont">
-        
+        <ContactForm />
+      </div>
+      <div className="contact-info">
+        <div className="contact-section">
+          <GmailIcon className="contact-icon" />
+          <a className="contact-text" href="mailto:albertus1291@gmail.com">
+            albertus1291@gmail.com
+          </a>
+        </div>
+        <div className="contact-section">
+          <LinkedinIcon className="contact-icon" />
+          <a
+            className="contact-text"
+            href="https://www.linkedin.com/in/albert-castro-albacete-38204615a/"
+          >
+            Linkedin
+          </a>
+        </div>
       </div>
     </div>
-	)
+  )
 }
 
 export default Contact
